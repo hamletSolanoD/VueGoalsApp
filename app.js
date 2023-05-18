cd9ac7bc6fbe0b231b41d1c560314aa348d17bf3
Hamlet <hamletsolanod@gmail.com>
2023-05-18 15:09:03 -0600

    simple chat

--- app.js
+++ app.js
@@ -5,14 +5,62 @@ const app = Vue.createApp({
         };
     },
     methods: {
-        onAdd() {
-            this.counter++;
+        onAdd(num) {
+            this.counter += num;
         },
-        onReduce() {
-            this.counter--;
+        onReduce(num) {
+            this.counter = this.counter > 0 ? this.counter - num : 0;
         }
     }
 
 });
 
-app.mount('#user-goal');
+app.mount('#user-counter');
+
+const app2 = Vue.createApp({
+    data() {
+        return {
+            goals: [],
+            newGoal: "",
+        };
+    },
+    methods: {
+        getGoal(event) {
+            this.newGoal = event.target.value;
+        },
+        addGoal() {
+            this.goals.push("<p>" + this.newGoal + "</p>");
+        }
+
+    }
+
+});
+
+app2.mount('#user-goal');
+
+const userChat = Vue.createApp({
+    data() {
+        return {
+            newText: "",
+            textBox: '',
+            userName: '',
+
+
+        };
+    },
+    methods: {
+        getText(event) {
+            this.newText = event.target.value;
+        },
+        setUserName(event) {
+            this.userName = event.target.value;
+        },
+        addText() {
+            this.textBox += this.userName + ":" + this.newText + "\n";
+        }
+
+    }
+
+});
+
+userChat.mount('#user-chat');
