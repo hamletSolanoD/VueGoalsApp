a6b6786e03ca835c66b44e55127956ec6b9480ac
Hamlet <hamletsolanod@gmail.com>
2023-05-18 13:52:41 -0600

    add and reduce in vue

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const app = Vue.createApp();
+const app = Vue.createApp({
+    data() {
+        return {
+            counter: 0,
+        };
+    },
+    methods: {
+        onAdd() {
+            this.counter++;
+        },
+        onReduce() {
+            this.counter--;
+        }
+    }
 
-app.mount('user-goal');
+});
+
+app.mount('#user-goal');
